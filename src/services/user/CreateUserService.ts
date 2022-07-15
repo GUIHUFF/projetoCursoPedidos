56de58a8ff8f5a67d5ce39ed66c9113b851975fc
Guilherme Huff <ghuff@alunos.utfpr.edu.br>
2022-07-15 18:16:20 -0300

    Add DB

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from '../../prisma'
+
 interface UserRequest {
   name: string;
   email: string;
@@ -6,7 +8,42 @@ interface UserRequest {
 
 class CreateUserService {
   async execute({ name, email, password }: UserRequest) {
-    return {name, email}
+    
+    //verificar se recebeu um email
+    if(!email){
+      throw new Error("Email incorrect");
+    }
+    //verificar se recebeu uma senha
+    if(!password){
+      throw new Error("Password incorrect");
+    }
+    
+
+    //verificar se já está cadastrado
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where:{
+        email: email
+      }
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      }, 
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      }
+    });
+
+    return user;
   }
 }
 
