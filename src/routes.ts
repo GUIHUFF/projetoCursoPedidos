39bb05eb30083ad66a039f087b2f80737f1c3e3e
Guilherme Huff <ghuff@alunos.utfpr.edu.br>
2022-07-20 12:47:22 -0300

    Listar Categorias

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import {Router, Request, Response} from 'express';
+
+import { isAuthenticated } from './middlewares/isAuthenticated';
+
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
 
-import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
@@ -16,5 +19,6 @@ router.get('/my', isAuthenticated, new DetailUserController().handle);
 
 // ROTAS CATEGORY
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
+router.get('/categorys', isAuthenticated, new ListCategoryController().handle);
 
 export { router };
