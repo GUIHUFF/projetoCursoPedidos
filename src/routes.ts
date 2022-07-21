b352e536bd182f5f695a4d6e5d4d82f0fbcb4f22
Guilherme Huff <ghuff@alunos.utfpr.edu.br>
2022-07-21 08:45:27 -0300

    list product by category

--- src/routes.ts
+++ src/routes.ts
@@ -9,9 +9,12 @@ import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+
 import { CreateProductController } from './controllers/product/CreateProductController';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
 import uploadConfig from './config/multer';
+
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -27,6 +30,6 @@ router.get('/categorys', isAuthenticated, new ListCategoryController().handle);
 
 // ROTAS PRODUCT
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
-
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
 
 export { router };
